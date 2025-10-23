a907940f32af798e4d19e1ec59e33a3dc02fa0df
KyoTranKMA <quangdieuisme@gmail.com>
2025-10-23 23:45:14 +0700

    done test cases

---
+++ Issue-Tracker-Challenge/server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+require('dotenv').config({path: './.env'});
+
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+
+let app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.route('/:project/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/issue.html');
+  });
+
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+
+fccTestingRoutes(app);
+
+//Routing API 
+apiRoutes(app);  
+    
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        let error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 3500);
+  }
+});
+
+module.exports = app; 
